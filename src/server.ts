e5aac6f5421ceb283a3d1434c6dc1df6d6115702
Nilton Sousa <nilton.jsousa@hotmail.com>
2022-11-01 08:21:28 -0300

    feat: returns a pool count on success

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const app = fastify({
     logger: true,
   });
 
-  app.get("/pools/count", () => {
-    return { count: 0 };
+  app.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
   });
 
   await app.listen({ port: 8080 });
